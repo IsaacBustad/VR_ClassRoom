6bd87cc042bbafcc16ba597b29ff04b2712eed58
Aaron Williams <akaaronwilliams@gmail.com>
2025-03-16 20:34:22 -0500

    Room Generation
    Needs to be smoother to move a selected point, will work on it throughout the week.
    Radial menu is almost done which will enable the selection of room creator, object placer, etc.

---
+++ Assets/Aaron/Scripts/RoomCreation/MeshGenerator.cs
@@ -0,0 +1,122 @@
+// Written by Aaron Williams
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MeshGenerator
+{
+    public static GameObject GenerateFlatMesh(List<Vector3> vertices, Material material, string name)
+    {
+        GameObject flatObject = new GameObject(name);
+        flatObject.layer = LayerMask.NameToLayer("Floor");
+
+        MeshFilter meshFilter = flatObject.AddComponent<MeshFilter>();
+        MeshCollider meshCollider = flatObject.AddComponent<MeshCollider>();
+        MeshRenderer meshRenderer = flatObject.AddComponent<MeshRenderer>();
+
+        meshRenderer.material = material;
+
+        Mesh mesh = new();
+
+        int[] triangles = new int[(vertices.Count - 2) * 3];
+        for (int i = 0; i < vertices.Count - 2; i++)
+        {
+            // Magic
+            if (name.Equals(XRRoomGenerator.CEILING_MESH_NAME, StringComparison.OrdinalIgnoreCase))
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 2;
+                triangles[i * 3 + 2] = i + 1;
+            }
+            else
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+            // End Magic
+        }
+
+        Vector2[] uv = new Vector2[vertices.Count];
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            uv[i] = new Vector2(vertices[i].x, vertices[i].z);
+        }
+
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles;
+        mesh.uv = uv;
+
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+
+        meshFilter.mesh = mesh;
+        meshCollider.sharedMesh = mesh;
+
+        return flatObject;
+
+       
+    }
+
+    public static List<GameObject> GenerateWallMeshes(List<Vector3> floorPoints, Material wallMaterial, float wallHeight)
+    {
+        List<GameObject> wallObjects = new List<GameObject>();
+
+        int wallsCount = floorPoints.Count;
+
+        for (int i = 0; i < wallsCount; i++)
+        {
+            int next = (i + 1) % wallsCount;
+            Vector3 floorPointA = floorPoints[i];
+            Vector3 floorPointB = floorPoints[next];
+
+            GameObject wallObject = new GameObject("Wall");
+            wallObject.layer = LayerMask.NameToLayer("Walls");
+
+            MeshFilter meshFilter = wallObject.AddComponent<MeshFilter>();
+            MeshCollider meshCollider = wallObject.AddComponent<MeshCollider>();
+            MeshRenderer meshRenderer = wallObject.AddComponent<MeshRenderer>();
+
+            meshRenderer.material = wallMaterial;
+
+            Mesh mesh = new();
+
+            // Magic
+            Vector3[] vertices = new Vector3[4]
+            {
+                new(floorPointA.x, 0, floorPointA.z), // Bottom-left
+                new(floorPointB.x, 0, floorPointB.z), // Bottom-right
+                new(floorPointB.x, wallHeight, floorPointB.z), // Top-right
+                new(floorPointA.x, wallHeight, floorPointA.z)  // Top-left
+            };
+
+            int[] triangles = new int[6] { 0, 2, 1, 0, 3, 2 };
+
+            float wallLength = Vector3.Distance(floorPointA, floorPointB);
+
+            Vector2[] uv = new Vector2[4]
+            {
+                new(0, 0),
+                new(wallLength, 0),
+                new(wallLength, 1),
+                new(0, 1)
+            };
+            // End Magic
+
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+            mesh.uv = uv;
+
+            mesh.RecalculateNormals();
+            mesh.RecalculateBounds();
+
+            meshFilter.mesh = mesh;
+            meshCollider.sharedMesh = mesh;
+
+            wallObjects.Add(wallObject);
+        }
+
+        return wallObjects;
+    }
+}
---
+++ Assets/Aaron/Scripts/RoomCreation/XRRoomGenerator.cs
@@ -0,0 +1,344 @@
+// Written by Aaron Williams
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XRRoomGenerator : MonoBehaviour
+{
+    [Header("Floor Point Settings")]
+    [SerializeField] private GameObject floorPointPrefab;
+
+    [Header("Controller Settings")]
+    [SerializeField] private Transform controllerTransform;
+    [SerializeField] private OVRInput.Controller ovrController = OVRInput.Controller.RTouch;
+    [SerializeField] private OVRInput.Button targetLineActivationButton = OVRInput.Button.PrimaryHandTrigger;
+    [SerializeField] private OVRInput.Button placeFloorPointButton = OVRInput.Button.PrimaryIndexTrigger;
+    [SerializeField] private OVRInput.Button generateRoomButton = OVRInput.Button.One;
+
+    [Header("Target LineRenderer Settings")]
+    [SerializeField] private float maxTargetLineDistanceNoPointSelected = 10f;
+    [SerializeField] private float maxTargetLineDistancePointSelected = 100f;
+    [SerializeField] private float targetLineWidth = 0.01f;
+    [SerializeField] private Color validTargetPlacementColor = Color.green;
+    [SerializeField] private Color invalidTargetColor = Color.red;
+    [SerializeField] private Color validTargetSelectionColor = Color.yellow;
+
+    [Header("Edge LineRenderer Settings")]
+    [SerializeField] private Color edgeLineColor = Color.blue;
+    [SerializeField] private float edgeLineWidth = 0.03f;
+
+    [Header("Selection Padding")]
+    [SerializeField] private float floorPointSelectionPadding = 0.2f;
+    [SerializeField] private float edgeSelectionPadding = 0.2f;
+
+    [Header("Mesh Generation Settings")]
+    [SerializeField] private float wallHeight = 2.5f;
+    [SerializeField] private Material floorMaterial;
+    [SerializeField] private Material wallMaterial;
+    [SerializeField] private Material ceilingMaterial;
+
+    private const string EDGE_LINE_RENDERER_NAME = "Edge LineRenderer";
+    private LineRenderer targetLineRenderer;
+    private LineRenderer edgeLineRenderer;
+    private Color currentTargetLineColor;
+
+    private List<Vector3> floorPoints = new();
+    private List<GameObject> floorPointPrefabs = new();
+
+    public const string FLOOR_MESH_NAME = "Floor Mesh";
+    public const string CEILING_MESH_NAME = "Ceiling Mesh";
+
+    private GameObject floorGameObject;
+    private List<GameObject> wallGameObjects = new();
+    private GameObject ceilingGameObject;
+
+    private bool isTargetValid = false;
+    private Vector3 targetHitPoint = Vector3.zero;
+
+    private int selectedPointIndex = -1;
+    private GameObject selectedSphere = null;
+
+    private bool isTargetOnEdge = false;
+    private int edgeStartIndex = -1;
+    private int edgeEndIndex = -1;
+    private Vector3 edgeStartPoint;
+    private Vector3 edgeEndPoint;
+
+    private int movingPointLayerMask;
+
+    private void Start()
+    {
+        targetLineRenderer = InitializeLineRenderer(targetLineWidth, invalidTargetColor, false);
+        edgeLineRenderer = InitializeLineRenderer(edgeLineWidth, edgeLineColor, true, EDGE_LINE_RENDERER_NAME);
+        movingPointLayerMask = ~(LayerMask.GetMask("Floor", "Walls"));
+    }
+
+    private void Update()
+    {
+        // TODO: Integrate input into Isaac's input
+        if (OVRInput.Get(targetLineActivationButton, ovrController))
+        {
+            UpdateTargetLine();
+        }
+        else
+        {
+            DeactivateTargetLine();
+        }
+        if (OVRInput.GetDown(placeFloorPointButton, ovrController))
+        {
+            HandlePlaceSelectPoint();
+        }
+        if (OVRInput.GetUp(placeFloorPointButton, ovrController))
+        {
+            if(selectedPointIndex >= 0)
+            {
+                DeselectPoint();
+            }
+        }
+        if (OVRInput.GetDown(generateRoomButton, ovrController))
+        {
+            GenerateRoom();
+        }
+        
+        if (selectedPointIndex >= 0)
+        {
+            UpdateSelectedPointPosition();
+        }
+    }
+
+    private LineRenderer InitializeLineRenderer(float width, Color color, bool isDistinctGameObject, string gameObjectName = null)
+    {
+        LineRenderer lineRenderer = isDistinctGameObject ? new GameObject(gameObjectName).AddComponent<LineRenderer>() : gameObject.AddComponent<LineRenderer>();
+        if (isDistinctGameObject) { lineRenderer.transform.parent = transform; }
+
+        lineRenderer.startWidth = width;
+        lineRenderer.endWidth = width;
+        lineRenderer.material.color = color;
+        lineRenderer.enabled = false;
+
+        return lineRenderer;
+    }
+
+    private void UpdateTargetLine()
+    {
+        targetLineRenderer.enabled = true;
+        edgeLineRenderer.enabled = false;
+        isTargetValid = false;
+
+        bool isPointNearby = false;
+
+        Vector3 targetLineOrigin = controllerTransform.position;
+        Vector3 targetLineDirection = controllerTransform.forward;
+
+        int layerMask;
+        float maxTargetDistance;
+        if (selectedPointIndex >= 0)
+        {
+            maxTargetDistance = maxTargetLineDistancePointSelected;
+            layerMask = movingPointLayerMask;
+        }
+        else
+        {
+            maxTargetDistance = maxTargetLineDistanceNoPointSelected;
+            layerMask = -1;
+        }
+
+        targetHitPoint = targetLineOrigin + targetLineDirection * maxTargetDistance;
+
+        if (Physics.Raycast(targetLineOrigin, targetLineDirection, out RaycastHit hit, maxTargetDistance, layerMask))
+        {
+            targetHitPoint = hit.point;
+
+            isTargetValid = hit.point.y < 0.01f;
+
+            if (selectedPointIndex < 0 && floorGameObject != null && floorPoints.Count >= 3)
+            {
+                isPointNearby = IsFloorPointNearHit(targetHitPoint);
+                if (!isPointNearby) { CheckForEdgeHit(targetHitPoint); }
+            }
+        }
+
+        currentTargetLineColor = (isPointNearby || selectedPointIndex >= 0) ? validTargetSelectionColor : (isTargetValid ? validTargetPlacementColor : invalidTargetColor);
+        UpdateLineRenderer(targetLineRenderer, targetLineOrigin, targetHitPoint, currentTargetLineColor);
+    }
+
+    private void DeactivateTargetLine()
+    {
+        targetLineRenderer.enabled = false;
+        edgeLineRenderer.enabled = false;
+        DeselectPoint();
+    }
+
+    private void UpdateSelectedPointPosition()
+    {
+        if (isTargetValid)
+        {
+            floorPoints[selectedPointIndex] = new Vector3(targetHitPoint.x, 0, targetHitPoint.z);
+            selectedSphere.transform.position = targetHitPoint;
+
+            RegenerateRoom();
+        }
+    }
+
+    private void HandlePlaceSelectPoint()
+    {
+        selectedPointIndex = TrySelectClosestPoint();
+
+        if (selectedPointIndex >= 0)
+        {
+            selectedSphere = floorPointPrefabs[selectedPointIndex];
+        }
+        else if (isTargetOnEdge)
+        {
+            InsertFloorPointOnEdge();
+        }
+        else if (isTargetValid)
+        {
+            CreateFloorPoint();
+        }
+    }
+
+    private int TrySelectClosestPoint()
+    {
+        int closestIndex = -1;
+        float closestDistance = floorPointSelectionPadding;
+
+        for (int i = 0; i < floorPoints.Count; i++)
+        {
+            float distanceToHit = Vector3.Distance(floorPoints[i], targetHitPoint);
+            if (distanceToHit < closestDistance)
+            {
+                closestDistance = distanceToHit;
+                closestIndex = i;
+            }
+        }
+
+        return closestIndex;
+    }
+
+    private void DeselectPoint()
+    {
+        selectedPointIndex = -1;
+        selectedSphere = null;
+    }
+
+    private void RegenerateRoom()
+    {
+        if (floorGameObject != null && wallGameObjects != null && floorPoints.Count >= 3)
+        {
+            GenerateRoom();
+        }
+    }
+
+    private void GenerateRoom()
+    {
+        if (floorPoints.Count >= 3)
+        {
+            if (floorGameObject != null) { Destroy(floorGameObject); }
+            if (ceilingGameObject != null) { Destroy(ceilingGameObject); }
+
+            if (wallGameObjects != null)
+            {
+                foreach (GameObject wall in wallGameObjects)
+                {
+                    if (wall != null) { Destroy(wall); }
+                }
+            }
+
+            List<Vector3> ceilingVertices = new();
+            foreach (Vector3 point in floorPoints)
+            {
+                ceilingVertices.Add(new Vector3(point.x, wallHeight, point.z));
+            }
+
+            ceilingGameObject = MeshGenerator.GenerateFlatMesh(ceilingVertices, ceilingMaterial, CEILING_MESH_NAME);
+            floorGameObject = MeshGenerator.GenerateFlatMesh(floorPoints, floorMaterial, FLOOR_MESH_NAME);
+            wallGameObjects = MeshGenerator.GenerateWallMeshes(floorPoints, wallMaterial, wallHeight);
+        }
+    }
+
+    private bool IsFloorPointNearHit(Vector3 hitPoint)
+    {
+        foreach (Vector3 floorPoint in floorPoints)
+        {
+            if (Vector3.Distance(floorPoint, hitPoint) < floorPointSelectionPadding) { return true; }
+        }
+        return false;
+    }
+
+    private void UpdateLineRenderer(LineRenderer lineRenderer, Vector3 start, Vector3 end, Color color)
+    {
+        lineRenderer.SetPositions(new Vector3[] { start, end });
+        lineRenderer.material.color = color;
+    }
+    private void CreateFloorPoint()
+    {
+        floorPoints.Add(targetHitPoint);
+        floorPointPrefabs.Add(Instantiate(floorPointPrefab, targetHitPoint, Quaternion.identity));
+    }
+
+    private void CheckForEdgeHit(Vector3 hitPoint)
+    {
+        edgeLineRenderer.enabled = false;
+        isTargetOnEdge = false;
+        edgeStartIndex = -1;
+        edgeEndIndex = -1;
+
+        float closestDistance = edgeSelectionPadding;
+
+        for (int i = 0; i < floorPoints.Count; i++)
+        {
+            int nextIndex = (i + 1) % floorPoints.Count;
+
+            Vector3 edgeStartPoint = floorPoints[i];
+            Vector3 edgeEndPoint = floorPoints[nextIndex];
+            Vector3 edge = edgeEndPoint - edgeStartPoint;
+            Vector3 edgeDirection = edge / edge.magnitude;
+
+            // Magic
+            Vector3 edgeStartPointToHitPoint = hitPoint - edgeStartPoint;
+            float projection = Vector3.Dot(edgeStartPointToHitPoint, edgeDirection);
+            if (projection < 0 || projection > edge.magnitude) { continue; }
+            Vector3 closestPoint = edgeStartPoint + edgeDirection * projection;
+            // End Magic
+
+            float distance = Vector3.Distance(hitPoint, closestPoint);
+
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                edgeStartIndex = i;
+                edgeEndIndex = nextIndex;
+                isTargetOnEdge = true;
+
+                this.edgeStartPoint = edgeStartPoint;
+                this.edgeEndPoint = edgeEndPoint;
+
+                edgeLineRenderer.enabled = true;
+                UpdateLineRenderer(edgeLineRenderer, new Vector3(edgeStartPoint.x, 0.01f, edgeStartPoint.z), new Vector3(edgeEndPoint.x, 0.01f, edgeEndPoint.z), edgeLineColor);
+            }
+        }
+    }
+
+    private void InsertFloorPointOnEdge()
+    {
+        Vector3 edgeStartPoint = floorPoints[edgeStartIndex];
+        Vector3 edgeEndPoint = floorPoints[edgeEndIndex];
+        Vector3 edge = edgeEndPoint - edgeStartPoint;
+        Vector3 edgeDirection = edge / edge.magnitude;
+
+        // Magic
+        float projection = Vector3.Dot(targetHitPoint - edgeStartPoint, edgeDirection);
+        Vector3 pointPosition = edgeStartPoint + edgeDirection * projection;
+        int insertIndex = (edgeStartIndex < edgeEndIndex) ? edgeEndIndex : (edgeEndIndex == 0) ? floorPoints.Count : edgeEndIndex;
+        // End Magic
+
+        floorPoints.Insert(insertIndex, new Vector3(pointPosition.x, 0, pointPosition.z));
+        floorPointPrefabs.Insert(insertIndex, Instantiate(floorPointPrefab, pointPosition, Quaternion.identity));
+
+        RegenerateRoom();
+
+        edgeStartIndex = -1;
+        edgeEndIndex = -1;
+    }
+}
