6c742869e2c258da8b3f4bd8f87ccf36a446a0d4
IsaacBustad <ijbbustad@gmail.com>
2025-04-13 11:42:14 -0500

    s
    s

--- Assets/Scripts/Factory/VR_PlacableItemPlacerGun.cs
+++ Assets/Scripts/Factory/VR_PlacableItemPlacerGun.cs
@@ -7,6 +7,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Oculus.Interaction;
+using Oculus.Interaction.HandGrab;
 
 public class VR_PlacableItemPlacerGun : PlacableItemPlacer
 {
