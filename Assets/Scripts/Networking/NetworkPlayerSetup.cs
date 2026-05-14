44a43a2a6e0173fcce2f716b9afc6267cc991f16
IsaacBustad <ijbbustad@gmail.com>
2026-05-14 11:30:10 -0500

    test 1
    test 1

---
+++ Assets/Scripts/Networking/NetworkPlayerSetup.cs
@@ -0,0 +1,49 @@
+// Created By   :   Isaac
+// Created      :   5/14/2026
+// Gemini Assisted
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Mirror;
+
+using Mirror;
+using UnityEngine;
+
+public class VRPlayerSetup : NetworkBehaviour
+{
+    [SerializeField] private GameObject localVRRig;  // Camera, tracking, etc.
+    [SerializeField] private GameObject remoteAvatar; // Visuals for other players
+
+    // Mirror fires this strictly on the client that OWNS this player object
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+        SetupLocalPlayer();
+    }
+
+    // Mirror fires this on ALL clients when any player object wakes up/spawns
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        
+        // If it's NOT the local player, set up the remote visuals
+        if (!isLocalPlayer)
+        {
+            SetupRemotePlayer();
+        }
+    }
+
+    private void SetupLocalPlayer()
+    {
+        localVRRig.SetActive(true);
+        remoteAvatar.SetActive(false);
+    }
+
+    private void SetupRemotePlayer()
+    {
+        localVRRig.SetActive(false);
+        remoteAvatar.SetActive(true);
+    }
+}
