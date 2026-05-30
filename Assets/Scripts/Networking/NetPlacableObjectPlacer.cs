b329e0e6bcc7db1f87d1faf4d3cfbb9450ad892c
isaacbustad <78563043+isaacbustad@users.noreply.github.com>
2026-05-30 17:25:04 -0500

    s
    s

---
+++ Assets/Scripts/Networking/NetPlacableObjectPlacer.cs
@@ -0,0 +1,15 @@
+// Created By   :   Isaac Bustad
+// Created      :   5/30/2026
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BugFreeProductions.Tools
+{
+    public class NetPlacableObjectPlacer : PlacableItemPlacer
+    {
+        
+    }
+}
